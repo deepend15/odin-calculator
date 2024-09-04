feba539cc7a02077ce8cf007e4e888642c26cd63
deepend15 <170750487+deepend15@users.noreply.github.com>
2024-09-04 17:39:54 -0400

    Show clicked numbers in display
    -Add code so that when a number button is clicked, that number will
    populate in the display
    -Store the current display number in a variable
    -Hide display overflow

--- javascript.js
+++ javascript.js
@@ -30,3 +30,29 @@ function operate(operator, num1, num2) {
     }
 }
 
+const acButton = document.querySelector(".ac-button");
+const numberButtons = document.querySelectorAll(".number-button");
+const divideButton = document.querySelector(".divide-button");
+const timesButton = document.querySelector(".times-button");
+const minusButton = document.querySelector(".minus-button");
+const equalsButton = document.querySelector(".equals-button");
+const plusButton = document.querySelector(".plus-button");
+
+let displayNumberArray = [];
+let displayValue = 0;
+
+function populateDisplay(e) {
+    const displayText = document.querySelector(".display-text");
+    if (displayText.textContent === "0" && e.target.textContent === "0") {
+        return;
+    } else {
+        displayNumberArray.push(e.target.textContent);
+        let displayNumberString = displayNumberArray.join("");
+        displayText.textContent = displayNumberString;
+        displayValue = Number(displayNumberString);
+    };
+}
+
+numberButtons.forEach((button) => {
+    button.addEventListener("click", populateDisplay);
+}); 
